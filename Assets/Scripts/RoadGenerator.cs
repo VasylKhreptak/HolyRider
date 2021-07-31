9346e092f158ae4d8a2ea15a3b5f47f5e70e1022
VasylKhreptak <vasylkhreptak12@gmail.com>
2021-07-31 12:14:16 +0300

    Road bumpiness;

---
+++ Assets/Scripts/RoadGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
